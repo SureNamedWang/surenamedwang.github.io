a342b2df963824d24fb4a5253359ef17fe053986
SureNamedWang <alphabetasatu@hotmail.com>
2020-10-02 09:40:41 +0700

    Deleted

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-import 'bootstrap';
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.sendFile(__dirname+'index.html');
-})
